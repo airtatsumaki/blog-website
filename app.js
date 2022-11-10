cf05f8842472fb069f9c85ff47c3c044ab7fa935
tatsu <naz1mb20@gmail.com>
2022-11-10 11:39:48 +0000

    setup files

---
+++ app.js
@@ -0,0 +1,24 @@
+import express from 'express';
+//__dirname for esm 
+import path from 'path';
+import {fileURLToPath} from 'url';
+const app = express();
+// instead of body-parser
+app.use(express.urlencoded({extended: true})); 
+app.use(express.json());
+
+app.set('view engine', 'ejs');
+app.use(express.static("public"));
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const homeStartingContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+const aboutContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Id faucibus nisl tincidunt eget nullam non nisi est. Tortor at auctor urna nunc id. Risus feugiat in ante metus dictum at tempor commodo ullamcorper. Odio morbi quis commodo odio aenean sed adipiscing diam donec. Nam aliquam sem et tortor consequat id porta nibh. Facilisi morbi tempus iaculis urna id. Ut porttitor leo a diam sollicitudin tempor id. Tristique sollicitudin nibh sit amet. Et odio pellentesque diam volutpat commodo sed egestas egestas. Consequat id porta nibh venenatis cras sed. Nibh sed pulvinar proin gravida hendrerit. Non odio euismod lacinia at. Platea dictumst vestibulum rhoncus est. Eros in cursus turpis massa tincidunt dui ut ornare lectus. Condimentum id venenatis a condimentum vitae sapien pellentesque. Elementum nibh tellus molestie nunc. Vivamus at augue eget arcu dictum varius duis at consectetur. Nullam eget felis eget nunc.";
+const contactContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Elit scelerisque mauris pellentesque pulvinar pellentesque habitant. Sed velit dignissim sodales ut. Pellentesque habitant morbi tristique senectus et netus. Nibh sed pulvinar proin gravida hendrerit. Velit egestas dui id ornare arcu odio. Adipiscing elit pellentesque habitant morbi tristique senectus et netus. Aliquet enim tortor at auctor urna nunc id. Tortor consequat id porta nibh venenatis cras sed. Consequat ac felis donec et odio pellentesque diam volutpat. Elit at imperdiet dui accumsan sit amet nulla facilisi morbi. Orci porta non pulvinar neque laoreet suspendisse. Cursus eget nunc scelerisque viverra mauris in aliquam sem fringilla. Id leo in vitae turpis massa sed elementum tempus. Ornare lectus sit amet est placerat. Maecenas accumsan lacus vel facilisis volutpat est velit. Urna nec tincidunt praesent semper feugiat nibh. Quis ipsum suspendisse ultrices gravida dictum fusce ut. Id semper risus in hendrerit gravida rutrum quisque.";
+
+app.get("/", (req, res) => {
+  res.render("home");
+})
+
+app.listen(process.env.PORT || 3000, () => console.log("Server is running on port 3000"));
