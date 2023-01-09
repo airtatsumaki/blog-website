308097b951968048985b2758e4f3be7a2dbd37d3
tatsu <naz1mb20@gmail.com>
2023-01-09 11:01:15 +0000

    v2 start

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 import express from "express";
 //__dirname for esm 
-import path from "path";
-import {fileURLToPath} from "url";
+// import path from "path";
+// import {fileURLToPath} from "url";
 import lowerCase from "lodash/lowerCase.js";
+import * as dotenv from 'dotenv';
+dotenv.config();
+import mongoose from 'mongoose';
+mongoose.set('strictQuery', true);
+
 const app = express();
 // instead of body-parser
 app.use(express.urlencoded({extended: true})); 
@@ -11,8 +16,23 @@ app.use(express.json());
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+
+const url = "mongodb+srv://naz:" + process.env.PASSWORD + "@cluster0.lgbc6oy.mongodb.net/blogsitev2";
+// const uri = `mongodb+srv://naz:${process.env.PASSWORD}@cluster0.lgbc6oy.mongodb.net/?retryWrites=true/todolistdb`;
+try{
+  await mongoose.connect(url);
+} catch (error) {
+  console.log(error);
+}
+
+const blogsSchema = new mongoose.Schema({
+  title : {type: String, required: true},
+  body : {type: String, required: true}
+});
+const Blog = mongoose.model("Blog", blogsSchema);
+
 
 const homeStartingContent = "HOME - Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 const aboutContent = "ABOUT - Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Id faucibus nisl tincidunt eget nullam non nisi est. Tortor at auctor urna nunc id. Risus feugiat in ante metus dictum at tempor commodo ullamcorper. Odio morbi quis commodo odio aenean sed adipiscing diam donec. Nam aliquam sem et tortor consequat id porta nibh. Facilisi morbi tempus iaculis urna id. Ut porttitor leo a diam sollicitudin tempor id. Tristique sollicitudin nibh sit amet. Et odio pellentesque diam volutpat commodo sed egestas egestas. Consequat id porta nibh venenatis cras sed. Nibh sed pulvinar proin gravida hendrerit. Non odio euismod lacinia at. Platea dictumst vestibulum rhoncus est. Eros in cursus turpis massa tincidunt dui ut ornare lectus. Condimentum id venenatis a condimentum vitae sapien pellentesque. Elementum nibh tellus molestie nunc. Vivamus at augue eget arcu dictum varius duis at consectetur. Nullam eget felis eget nunc.";
